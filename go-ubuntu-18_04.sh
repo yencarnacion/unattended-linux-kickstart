b3aa3c89cb1733e3679fabfbf9af22c70bd9b189
Yamir Encarnacion <yencarnacion@webninjapr.com>
2018-05-17 15:52:12 -0400

    changed hostname of bionic machine

--- go-ubuntu-18_04.sh
+++ go-ubuntu-18_04.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 virt-install \
---name=ubuntu1.example.com \
+--name=bionic1.example.com \
 --vcpus=1 \
 --memory=1024 \
 --nographics \
 --os-type=linux \
 --location=http://us.archive.ubuntu.com/ubuntu/dists/bionic/main/installer-amd64/ \
---disk path=/var/lib/libvirt/images/ubuntu1.example.com,size=10 \
+--disk path=/var/lib/libvirt/images/bionic1.example.com,size=10 \
 --initrd-inject=./ks/ubuntu-18_04/ks.cfg \
 --extra-args="ks=file:/ks.cfg  console=ttyS0,115200"
 
