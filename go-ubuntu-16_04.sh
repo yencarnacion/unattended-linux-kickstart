486a65f5df3e2a04d039260a8b235f7ee21a65b5
Yamir Encarnacion <yencarnacion@webninjapr.com>
2018-05-17 15:05:29 -0400

    changed disk name on xenial script

--- go-ubuntu-16_04.sh
+++ go-ubuntu-16_04.sh
@@ -7,7 +7,7 @@ virt-install \
 --nographics \
 --os-type=linux \
 --location=http://us.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/ \
---disk path=/var/lib/libvirt/images/ubuntu1.example.com,size=10 \
+--disk path=/var/lib/libvirt/images/xenial1.example.com,size=10 \
 --initrd-inject=./ks/ubuntu-16_04/ks.cfg \
 --extra-args="ks=file:/ks.cfg  console=ttyS0,115200"
 
